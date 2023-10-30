c2633a6595b8070b177968581f4ebb06b164da68
Rafael Almeida <debemrafael@gmail.com>
2023-10-29 23:43:56 -0300

    remove useless files

--- so.vert.glsl
+++
@@ -1,8 +0,0 @@
-// Vertex
-#version 450 core
-layout (location = 0) in vec2 pos;         // glVertex2f <-1,+1>
-out smooth vec2 p32;                    // texture end point <0,1>
-void main() {
-    p32 = pos;
-    gl_Position = vec4(pos, 0.0, 1.0);
-}
